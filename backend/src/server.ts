cf51efcd1a1411dcccf1bb300ac0e12f71894d3c
filipe <fhilips.souza@gmail.com>
2020-10-17 19:26:12 -0300

    Criando o backend

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,20 +1,21 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
 
 import './database/connection';
+
+import routes from './routes';
+import errorHandler from './errors/handler';
 
 const app = express();
 
 
-
-app.use(express.json())
-
-app.get('/users/:id', (request, reponse) => {
-
-
-
-
-    return reponse.json({message: "hello world"})
-
-});
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 app.listen(3333);
